ffb0a2f493d381426bf53e81b0d070ea57d2720a
TaoHung <ponder5247@gmail.com>
2023-09-21 08:31:53 +0800

    Change to Daily BackUp , Remove Weekly BackUp

--- backup.sh
+++ backup.sh
@@ -7,18 +7,14 @@ target_file="/media/ponder/ADATA HM900/BackUp/$today.tar.gz"
 google_dir="/home/ponder/migoogledrive/Stock"
 
 
-# 在目标文件夹中创建一个临时文件夹
 tmp_dir="/tmp/backup_temp"
 mkdir -p "$tmp_dir"
 
-# 复制需要压缩的文件到临时文件夹
 cp -r "$stock_dir" "$tmp_dir"
 cp -r "$op_dir" "$tmp_dir"
 
-# 使用 -C 选项来指定基本目录，然后压缩文件
 tar -czvf "$target_file" -C "$tmp_dir" .
 
-# 删除临时文件夹
 rm -r "$tmp_dir"
 
 if [ $? -eq 0 ]; then
@@ -37,4 +33,3 @@ else
     exit 1
 fi
 
-#!/bin/bash
