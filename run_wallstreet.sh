a15ea9ea0a3d3e4e8350a6d72c807aa3461c6246
TaoHung <ponder5247@gmail.com>
2023-10-03 18:17:52 +0800

    add stdout save to the path

--- run_wallstreet.sh
+++ run_wallstreet.sh
@@ -5,13 +5,13 @@ op_Log="/media/ponder/ADATA HM900/Log/opcal_log.txt"
 daily_Log="/media/ponder/ADATA HM900/Log/daily_log.txt"
 
 python_script="/home/ponder/Stock_Option_Analysis/wallstreetTest.py"
-python3 $python_script > "$DL_Log"
+python3 $python_script 2> "$DL_Log"
 git clean -df
 
 pyscript="/home/ponder/Stock_Option_Analysis/optionCal.py"
-python3 $pyscript > "$op_Log"
+python3 $pyscript 2> "$op_Log"
 git clean -df
 
 pyscript="/home/ponder/Stock_Option_Analysis/dailyReport.py"
-python3 $pyscript > "$daily_Log"
+python3 $pyscript 2> "$daily_Log"
 git clean -df
