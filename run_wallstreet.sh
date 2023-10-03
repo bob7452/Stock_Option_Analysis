c20c5b8e801ec94eb25722ec0beb7838df9cd2fb
TaoHung <ponder5247@gmail.com>
2023-10-03 20:09:51 +0800

    add save operation log

--- run_wallstreet.sh
+++ run_wallstreet.sh
@@ -5,13 +5,13 @@ op_Log="/media/ponder/ADATA HM900/Log/opcal_log.txt"
 daily_Log="/media/ponder/ADATA HM900/Log/daily_log.txt"
 
 python_script="/home/ponder/Stock_Option_Analysis/wallstreetTest.py"
-python3 $python_script 2> "$DL_Log"
+python3 $python_script > "$DL_Log" 2>&1
 git clean -df
 
 pyscript="/home/ponder/Stock_Option_Analysis/optionCal.py"
-python3 $pyscript 2> "$op_Log"
+python3 $pyscript 2> "$op_Log" 2>&1
 git clean -df
 
 pyscript="/home/ponder/Stock_Option_Analysis/dailyReport.py"
-python3 $pyscript 2> "$daily_Log"
+python3 $pyscript 2> "$daily_Log" 2>&1
 git clean -df
