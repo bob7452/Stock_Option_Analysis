0e2284038c27577cc0c85b75b4abb349222ce367
TaoHung <ponder5247@gmail.com>
2023-10-06 18:22:43 +0800

    [update] when today is holiday, skip once mission

---
+++ isHoliday.py
@@ -0,0 +1,16 @@
+import sys
+from pandas_market_calendars import get_calendar
+import Constant as myarg
+
+today = myarg.getToday(12)
+today = today.strftime('%Y-%m-%d')
+nyse = get_calendar('NYSE')
+
+
+is_holidays = nyse.valid_days(start_date=today, end_date=today)
+
+if is_holidays.empty:
+    sys.exit(1)
+else:
+    sys.exit(0)
+
